87f9281a1735b131f096dd891da08127b40e5cf5
Thorsten Käbisch <thorsten@kaebisch.de>
2025-06-10 19:22:32 +0200

    fix: update @angular/material version to use tilde and caret for flexibility

--- apps/miles/src/app/app.component.ts
+++ apps/miles/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
-    imports: [NxWelcomeComponent, RouterModule],
+    imports: [NxWelcomeComponent, RouterModule, MatSlideToggleModule],
     selector: 'flight-demo-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
