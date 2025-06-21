33ebbe3f8a57e77f58c94c7d5716996f75ad1620
tk-l <61744289+tk-l@users.noreply.github.com>
2025-06-21 23:13:09 +0000

    feat: implement login component with form fields and styles

--- apps/miles/src/app/app.routes.ts
+++ apps/miles/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Route } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 export const appRoutes: Route[] = [
-    { path: 'login', component: LoginComponent },    
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },    
 ];
--- apps/miles/src/app/login/login.component.ts
+++ apps/miles/src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'miles-login',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 }
