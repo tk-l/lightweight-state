3ba5e855cc09c420e333a72db40a83bca48b322e
tk-l <61744289+tk-l@users.noreply.github.com>
2025-06-23 21:36:08 +0000

    feat: implement sidenav layout with navigation items and styles

--- apps/miles/src/app/app.routes.ts
+++ apps/miles/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { LoginComponent } from './login/login.component';
 
 export const appRoutes: Route[] = [
     { path: 'login', component: LoginComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' },    
+    // { path: '', redirectTo: 'login', pathMatch: 'full' },    
 ];
