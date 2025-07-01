08e9c339c4fd54e17bce566e7a04618684e4800f
tk-l <61744289+tk-l@users.noreply.github.com>
2025-07-01 19:04:27 +0000

    feat: add welcome and dashboard components with routing and layout updates

--- apps/miles/src/app/app.routes.ts
+++ apps/miles/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Route } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const appRoutes: Route[] = [
-    { path: 'login', component: LoginComponent },
-    // { path: '', redirectTo: 'login', pathMatch: 'full' },    
+  {path: '', component: WelcomeComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  
+  { path: 'login', component: LoginComponent },
+  // Uncomment the following line to redirect to login by default
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },    
 ];
